02e50307ae3a986d4e917e051ef4a5aa15fd5fac
P0u4a <pouyamiria@gmail.com>
2023-09-03 00:43:51 +0930

    Initial design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,116 @@
-import Image from 'next/image'
-
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+import { Timer, LineChart, Zap } from 'lucide-react';
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <>
+            <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+                <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+                    <Link
+                        href={
+                            'https://chrome.google.com/webstore/category/extensions'
+                        }
+                        className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
+                        target="_blank"
+                    >
+                        Download for Chrome
+                    </Link>
+                    <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold pb-4">
+                        Track your Leetcode Progress with just one click
+                    </h1>
+                    <p className="max-w-xl leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+                        A chrome extension for logging leetcode questions and
+                        visualising your progress.
+                    </p>
+                    <div className="space-x-4">
+                        <Link
+                            href="/login"
+                            className={cn(buttonVariants({ size: 'lg' }))}
+                        >
+                            Get Started
+                        </Link>
+                        <Link
+                            href={'https://github.com/P0u4a'}
+                            target="_blank"
+                            rel="noreferrer"
+                            className={cn(
+                                buttonVariants({
+                                    variant: 'outline',
+                                    size: 'lg',
+                                })
+                            )}
+                        >
+                            GitHub
+                        </Link>
+                    </div>
+                </div>
+            </section>
+            <section
+                id="features"
+                className="container space-y-6 py-8 md:py-12 lg:py-24"
+            >
+                <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+                    <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl font-bold">
+                        Features
+                    </h2>
+                </div>
+                <div className="flex flex-col justify-center gap-4 mx-auto md:flex-row md:max-w-[64rem]">
+                    <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+                        <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+                            <Timer />
+                            <div className="space-y-2">
+                                <h3 className="font-bold">Stopwatch</h3>
+                                <p className="text-sm text-muted-foreground">
+                                    Non-intrusive timer for conveniently
+                                    tracking your solve times
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+                        <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+                            <Zap />
+                            <div className="space-y-2">
+                                <h3 className="font-bold">Auto Logging</h3>
+                                <p className="text-sm text-muted-foreground">
+                                    No manual input needed, log your solved
+                                    problems and times with a single click.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+                        <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+                            <LineChart />
+                            <div className="space-y-2">
+                                <h3 className="font-bold">
+                                    Data Visualisation
+                                </h3>
+                                <p className="text-sm text-muted-foreground">
+                                    Visualise your progress through the
+                                    auto-generated graphs based on your solve
+                                    times.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section
+                id="open-source"
+                className="container py-8 md:py-12 lg:py-24"
+            >
+                <div className="mx-auto flex max-w-[58rem] flex-col items-center justify-center gap-4 text-center">
+                    <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl font-bold pb-2">
+                        Begin Solving Today
+                    </h2>
+                    <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+                        Tracking your Leetcode progress is a tried and tested
+                        way to achieve success.
+                    </p>
+                </div>
+            </section>
+        </>
+    );
 }
