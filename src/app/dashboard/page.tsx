d96cf8bf29236e14823d588239ec24c9b8dd3094
P0u4a <pouyamiria@gmail.com>
2023-09-07 00:53:49 +0930

    More tabs + responsiveness

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,27 +9,35 @@ export default function Dashboard() {
                         Dashboard
                     </h1>
 
-                    <Tabs defaultValue="solved-problems" className="w-[24rem]">
-                        <TabsList className="grid w-full grid-cols-3">
+                    <Tabs defaultValue="solved-problems" className="w-[32rem]">
+                        <TabsList className="grid w-[24rem] md:w-full grid-cols-2 md:grid-cols-4">
                             <TabsTrigger value="solved-problems">
                                 Solved Problems
                             </TabsTrigger>
                             <TabsTrigger value="time-graph">
                                 Time Graph
                             </TabsTrigger>
-                            <TabsTrigger value="statistics">
-                                Statistics
+                            <TabsTrigger value="diff-ratios">
+                                Difficulty Ratios
+                            </TabsTrigger>
+                            <TabsTrigger value="topic-freq">
+                                Topic Frequencies
                             </TabsTrigger>
                         </TabsList>
-                        <TabsContent value="solved-problems">
-                            Solved Problems Table here
-                        </TabsContent>
-                        <TabsContent value="time-graph">
-                            Time graph for each difficulty
-                        </TabsContent>
-                        <TabsContent value="statistics">
-                            Difficulty and topics (top ~10) percentages
-                        </TabsContent>
+                        <div className="pt-12">
+                            <TabsContent value="solved-problems">
+                                Solved Problems Table here
+                            </TabsContent>
+                            <TabsContent value="time-graph">
+                                Time graph for each difficulty
+                            </TabsContent>
+                            <TabsContent value="diff-ratios">
+                                Difficulty ratios as circular percentages
+                            </TabsContent>
+                            <TabsContent value="topic-freq">
+                                Topic frequencies as columns of loading bars
+                            </TabsContent>
+                        </div>
                     </Tabs>
                 </div>
             </section>
