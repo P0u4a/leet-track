59a9b7e78917f8cae67aa4cfda87be65ba4e5c84
P0u4a <pouyamiria@gmail.com>
2023-09-29 21:35:07 +0930

    Add initial privacy policy and tnc

--- src/app/(policies)/privacy/page.tsx
+++ src/app/(policies)/privacy/page.tsx
@@ -4,9 +4,69 @@ export default function Privacy() {
             <h1 className="text-3xl leading-[1.1] sm:text-3xl md:text-6xl font-bold">
                 Privacy Policy
             </h1>
-            <p className="max-w-xl leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-                This is the privacy policy.
-            </p>
+            <h3>
+                <strong>Last Updated:</strong> September 16, 2023
+            </h3>
+
+            <div className="flex flex-col gap-5 max-w-5xl leading-normal sm:text-xl sm:leading-8 text-justify">
+                <h2 className="font-bold">1. Introduction</h2>
+                <p>
+                    Welcome to LeetTrack&apos;s Privacy Policy. This Privacy
+                    Policy explains how your personal information is collected,
+                    used, and disclosed when you use our Service.
+                </p>
+
+                <h2 className="font-bold">2. Information We Collect</h2>
+                <p>
+                    LeetTrack collects information provided by users during the
+                    GitHub OAuth authentication process. This information may
+                    include your GitHub username and email address. We do not
+                    collect any additional personal information.
+                </p>
+
+                <h2 className="font-bold">3. Use of Your Information</h2>
+                <p>
+                    The information collected is used solely for the purpose of
+                    enabling the functionality of the LeetTrack app. We do not
+                    use your information for any other purpose.
+                </p>
+
+                <h2 className="font-bold">4. Data Storage</h2>
+                <p>
+                    Your personal information, including but not limited to your
+                    GitHub username and email address, is securely stored on the
+                    servers of our database provider, Planetscale. We take data
+                    security seriously and have implemented measures to protect
+                    your information from unauthorized access or disclosure.
+                </p>
+
+                <h2 className="font-bold">5. Disclosure of Your Information</h2>
+                <p>
+                    We do not disclose your personal information to any third
+                    parties, except as required by law.
+                </p>
+
+                <h2 className="font-bold">6. Security</h2>
+                <p>
+                    We take the security of your personal information seriously.
+                    We use industry-standard security measures to protect your
+                    data from unauthorized access or disclosure.
+                </p>
+
+                <h2 className="font-bold">7. Changes to this Privacy Policy</h2>
+                <p>
+                    We may update our Privacy Policy from time to time. Any
+                    changes will be posted on this page with a new effective
+                    date.
+                </p>
+
+                <h2 className="font-bold">8. Contact Us</h2>
+                <p>
+                    If you have any questions or concerns about this Privacy
+                    Policy or our data practices, please contact us at [Contact
+                    Email Address].
+                </p>
+            </div>
         </section>
     );
 }
--- src/app/(policies)/terms/page.tsx
+++ src/app/(policies)/terms/page.tsx
@@ -4,9 +4,73 @@ export default function Terms() {
             <h1 className="text-3xl leading-[1.1] sm:text-3xl md:text-6xl font-bold">
                 Terms and Conditions
             </h1>
-            <p className="max-w-xl leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-                This is the terms and conditions.
-            </p>
+            <h3>
+                <strong>Last Updated: September 16, 2023</strong>
+            </h3>
+            <div className="flex flex-col gap-5 max-w-5xl leading-normal sm:text-xl sm:leading-8 text-justify">
+                <h2 className="font-bold">1. Acceptance of Terms</h2>
+                <p>
+                    By accessing or using the Service, you affirm that you are
+                    of legal age to enter into this agreement, or if you are a
+                    minor, that you have obtained parental or guardian consent
+                    to use the Service.
+                </p>
+
+                <h2 className="font-bold">2. User Registration</h2>
+                <p>
+                    2.1. To access the Service, you must log in using your
+                    GitHub account credentials. You are responsible for
+                    maintaining the confidentiality of your login information.
+                </p>
+                <p>
+                    2.2. You agree to provide accurate, current, and complete
+                    information during the registration process and to update
+                    such information to keep it accurate, current, and complete.
+                </p>
+                <p>
+                    2.3. You may not share your login credentials or allow
+                    others to access your account.
+                </p>
+
+                <h2 className="font-bold">3. Service Description</h2>
+                <p>
+                    3.1. LeetTrack provides a Chrome extension that allows users
+                    to log their LeetCode questions directly from the extension.
+                </p>
+                <p>
+                    3.2. The logged questions will be displayed in a table on
+                    your dashboard in the web application, along with data
+                    visualizations such as time graphs, topic frequencies, and
+                    difficulty ratios.
+                </p>
+                <p>
+                    3.3. LeetTrack may update or modify the Service at its
+                    discretion and may add or remove features without prior
+                    notice.
+                </p>
+
+                <h2 className="font-bold">10. Governing Law</h2>
+                <p>
+                    These Terms shall be governed by and construed in accordance
+                    with the laws of Australia.
+                </p>
+
+                <h2 className="font-bold">11. Contact Information</h2>
+                <p>
+                    If you have any questions or concerns about these Terms,
+                    please contact us at [Contact Email Address].
+                </p>
+
+                <p>
+                    By using the LeetTrack Service, you acknowledge that you
+                    have read, understood, and agreed to these Terms and
+                    Conditions. LeetTrack reserves the right to modify or update
+                    these Terms at any time. It is your responsibility to review
+                    these Terms periodically for changes. Your continued use of
+                    the Service after any changes to these Terms will constitute
+                    your acceptance of those changes.
+                </p>
+            </div>
         </section>
     );
 }
