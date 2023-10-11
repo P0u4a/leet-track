ba1aa95a0b5fd2d0a1608ac0b880376fde40b355
P0u4a <pouyamiria@gmail.com>
2023-10-11 23:40:27 +1030

    Remove console log

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -36,7 +36,6 @@ export async function POST(req: Request) {
             'svix-signature': svix_signature,
         }) as WebhookEvent;
     } catch (err) {
-        console.error('Error verifying webhook:', err);
         return new Response('Error verifying webhook', {
             status: 400,
         });
