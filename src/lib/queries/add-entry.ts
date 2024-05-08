b60c313c5a81a6f791e7588da9e983ecb093ef81
P0u4a <pouyamiria@gmail.com>
2024-05-08 22:16:01 +0930

    Improve flexibility of title input

--- src/lib/queries/add-entry.ts
+++ src/lib/queries/add-entry.ts
@@ -1,11 +1,20 @@
 import type { NewEntry, AddEntry, LeetcodeRes } from '@/types/request';
 export async function addEntry(entry: NewEntry) {
+    const processTitle = (title: string) => {
+        const regex = /https:\/\/leetcode\.com\/problems\/([^/]+)\//;
+
+        const titleSlug = title.match(regex);
+        // If input was the question url, return the title slug, else strip out the number from the title string if it exists
+        return titleSlug ? titleSlug[1] : title.replace(/^\d+\.\s(.*)$/, '$1');
+    };
+
+    const rawTitle = processTitle(entry.title);
     const leetcodeRes = await fetch('/api/fetch-leetcode', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ title: entry.title }),
+        body: JSON.stringify({ title: rawTitle }),
     });
 
     if (leetcodeRes.status === 403) return 403;
